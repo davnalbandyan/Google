d4c9b9e82f44c7620d267f5955b57396bb8b7c60
davnalbandyan <davnalbandyan@gmail.com>
2024-04-01 23:41:12 +0400

    javascript for google search

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
 let searchBar = document.querySelector(".searchBar .search");// input text
 let btnX = document.querySelector('.searchBar .fa-x');  
 let searchGoogle = document.getElementById("searchGoogle") //search button
+let keybord = document.querySelector('.fa-regular');
 
+
+let google  = document.querySelector('.google ');
 
 
 searchBar.addEventListener("input", searchBarX);
@@ -32,9 +35,37 @@ function searchBarX() {
 searchGoogle.addEventListener("click",()=>{
     location.href = "https://www.google.ru/search?q=" + searchBar.value;
     searchBar.value = null;
-})
-    
+});
 
+
+let glass = document.querySelector('.fa-magnifying-glass');
+
+glass.addEventListener("click",()=>{
+    location.href = "https://www.google.ru/search?q=" + searchBar.value;
+    searchBar.value = null; 
+})
+
+let sunny = document.querySelector(".darkAndlight .fa-sun");
+let moon = document.querySelector(".darkAndlight .fa-moon");
+
+sunny.addEventListener("click",()=>{
+    google.style.background = "white";
+    sunny.style.display = "none";
+    moon.style.display = "block";
+    searchBar.style.border = "5px solid black";
+    searchBar.style.cssText = `background-color:white;color:black`
+
+    
+    
+});
+
+moon.addEventListener("click",()=>{
+    google.style.background = "#202124";
+    moon.style.display = "none";
+    sunny.style.cssText = `background:white, border-color:black`
+    searchBar.style.cssText = `background-color:202124;color:white`
+
+})
 
 
 
