f4dea2921f6c37c4b4bb074683ad8b4bb10bd600
davnalbandyan <davnalbandyan@gmail.com>
2024-04-02 10:30:49 +0400

    add the eventlistener to the glass,if the search bar is empty it can not do search & if have space also

--- index.js
+++ index.js
@@ -10,36 +10,9 @@ let google  = document.querySelector('.google ');
 
 
 searchBar.addEventListener("input", searchBarX);
-
-function searchBarX() {
-    
-    let searchValue = searchBar.value.toLowerCase();
-    if (searchValue) {
-        btnX.style.display = "block";
-    } else {
-        btnX.style.display = "none";
-    };
-    document.addEventListener("keydown",(e)=>{
-        if(e.key === "Enter"){
-            location.href = "https://www.google.ru/search?q=" + searchValue;
-            searchBar.value = " "
-        }
-    })
-     };
-
-function search(){
-    location.href = "https://www.google.ru/search?q=" + searchBar.value;
-    searchBar.value = " "
-
-}
-
 searchGoogle.addEventListener("click",search);
 
-
 let glass = document.querySelector('.fa-magnifying-glass');
-
-
-
 let sunny = document.querySelector(".darkAndlight .fa-sun");
 let moon = document.querySelector(".darkAndlight .fa-moon");
 
@@ -49,9 +22,6 @@ sunny.addEventListener("click",()=>{
     moon.style.display = "block";
     searchBar.style.border = "5px solid black";
     searchBar.style.cssText = `background-color:white;color:black`
-
-    
-    
 });
 
 moon.addEventListener("click",()=>{
@@ -60,8 +30,35 @@ moon.addEventListener("click",()=>{
     sunny.style.cssText = `background:white, border-color:black`
     searchBar.style.cssText = `background-color:202124;color:white`
 
+});
+
+
+glass.addEventListener("click",()=>{
+    let value = searchBar.value
+    if(value.trim().length > 0 ){
+        search()
+    }
 })
 
+function search(){
+    location.href = "https://www.google.ru/search?q=" + searchBar.value;
+    searchBar.value = " "
+
+};
+function searchBarX() {
+    let searchValue = searchBar.value.toLowerCase();
+   if (searchValue) {
+       btnX.style.display = "block";
+   } else {
+       btnX.style.display = "none";
+   };
+   document.addEventListener("keydown",(e)=>{
+       if(e.key === "Enter"){
+           location.href = "https://www.google.ru/search?q=" + searchValue;
+           searchBar.value = " "
+       }
+   })
+    };
 
 
 // google i aj koxmi pahy sarqi, pochta,karta, keybordy, u mek el vor im search aracnery pahi, ha mek el glass y nenc ara vor datark search chani
